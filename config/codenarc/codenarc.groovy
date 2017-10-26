7cfc7a04f9f8ea4557e80aad048dc95ca9605ad3
Andrii Plotnikov <plotnikovandrey1990@gmail.com>
2017-10-26 19:01:37 +0200

    Add gradle configuration

---
+++ config/codenarc/codenarc.groovy
@@ -0,0 +1,29 @@
+ruleset {
+    ruleset('rulesets/basic.xml')
+    ruleset('rulesets/braces.xml')
+    ruleset('rulesets/concurrency.xml')
+    ruleset('rulesets/convention.xml')
+    ruleset('rulesets/design.xml')
+    ruleset('rulesets/dry.xml') {
+        DuplicateStringLiteral(enabled: false)
+    }
+    ruleset('rulesets/enhanced.xml')
+    ruleset('rulesets/formatting.xml') {
+        ClassJavadoc(enabled: false)
+        SpaceAroundMapEntryColon(enabled: false)
+        LineLength(enabled: false)
+    }
+    ruleset('rulesets/generic.xml')
+    ruleset('rulesets/groovyism.xml')
+    ruleset('rulesets/exceptions.xml')
+    ruleset('rulesets/imports.xml')
+    ruleset('rulesets/size.xml')
+    ruleset('rulesets/unnecessary.xml') {
+        UnnecessaryBooleanExpression(enabled: false)
+        UnnecessaryGetter(enabled: false)
+    }
+    ruleset('rulesets/unused.xml') {
+        UnusedObject(enabled: false)
+    }
+    ruleset('rulesets/security.xml')
+}
