b1b40c9b89a952288cd01e11852151dab58b92cf
Andrii Plotnikov <plotnikovandrey1990@gmail.com>
2017-10-29 20:12:21 +0100

    Add workflow examples for Spock unit tests

--- src/test/groovy/io/github/aplotnikov/junit5_vs_spock/entities/ClientSpec.groovy
+++ src/test/groovy/io/github/aplotnikov/junit5_vs_spock/entities/ClientSpec.groovy
@@ -21,7 +21,24 @@ class ClientSpec extends Specification {
     String secondName = 'Plotnikov'
 
     @Subject
+    @Shared
     Client client = new Client(firstName, secondName, ['test@gmail.com', 'test2@gmail.com'])
+
+    void setupSpec() {
+        println 'Specification is prepared'
+    }
+
+    void cleanupSpec() {
+        println 'Specification preparation is destroyed'
+    }
+
+    void setup() {
+        println 'Unit test is prepared'
+    }
+
+    void cleanup() {
+        println 'Unit test preparation is destroyed'
+    }
 
     void 'client should have correct first name and second name'() {
         expect:
@@ -39,43 +56,6 @@ class ClientSpec extends Specification {
             }
     }
 
-    void 'client should be not able to take a loan when he has unknown status'() {
-        when:
-            client.takeLoan(TEN)
-        then:
-            IllegalStateException exception = thrown(IllegalStateException)
-            exception.message == 'In order to take a lona client should have status identified. Current status is UNKNOWN'
-    }
-
-    void 'client should be not able to take a loan when he has registered status'() {
-        given:
-            client.register()
-        when:
-            client.takeLoan(TEN)
-        then:
-            IllegalStateException exception = thrown(IllegalStateException)
-            exception.message == 'In order to take a lona client should have status identified. Current status is REGISTERED'
-    }
-
-    void 'client should not have enough money to take a loan'() {
-        given:
-            client.identify()
-        when:
-            client.takeLoan(TEN)
-        then:
-            IllegalStateException exception = thrown(IllegalStateException)
-            exception.message == 'Client does not have enough money'
-    }
-
-    void 'client should have enough money to take a loan'() {
-        given:
-            client.identify()
-        when:
-            client.takeLoan(ONE)
-        then:
-            noExceptionThrown()
-    }
-
     @Ignore
     void 'test should be ignored'() {
         expect:
@@ -85,6 +65,11 @@ class ClientSpec extends Specification {
     void 'client should have correct e-mail addresses'() {
         expect:
             client.emails == ['test@gmail.com', 'test2@gmail.com']
+    }
+
+    void 'client should have status unknown'() {
+        expect:
+            client.unknown
     }
 
     @Timeout(2)
@@ -98,5 +83,50 @@ class ClientSpec extends Specification {
             EqualsVerifier.forClass(Client)
                     .withIgnoredFields('status')
                     .verify()
+    }
+
+    void 'client should be not able to take a loan when he has unknown status'() {
+        when:
+            client.takeLoan(TEN)
+        then:
+            IllegalStateException exception = thrown(IllegalStateException)
+            exception.message == 'In order to take a lona client should have status identified. Current status is UNKNOWN'
+    }
+
+    void 'client should have status registered when registration is completed'() {
+        when:
+            client.register()
+        then:
+            client.registered
+    }
+
+    void 'client should be not able to take a loan when he has registered status'() {
+        when:
+            client.takeLoan(TEN)
+        then:
+            IllegalStateException exception = thrown(IllegalStateException)
+            exception.message == 'In order to take a lona client should have status identified. Current status is REGISTERED'
+    }
+
+    void 'client should have status identified when identification is completed'() {
+        when:
+            client.identify()
+        then:
+            client.identified
+    }
+
+    void 'client should not have enough money to take a loan'() {
+        when:
+            client.takeLoan(TEN)
+        then:
+            IllegalStateException exception = thrown(IllegalStateException)
+            exception.message == 'Client does not have enough money'
+    }
+
+    void 'client should have enough money to take a loan'() {
+        when:
+            client.takeLoan(ONE)
+        then:
+            noExceptionThrown()
     }
 }
