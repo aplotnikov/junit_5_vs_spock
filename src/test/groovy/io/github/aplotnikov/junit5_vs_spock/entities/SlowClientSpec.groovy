e8be64b5ce60946abad41a05cfd46ed38b70171a
Andrii Plotnikov <plotnikovandrey1990@gmail.com>
2017-10-29 22:42:09 +0100

    Add examples verifying asynchronous operation in Spock

--- src/test/groovy/io/github/aplotnikov/junit5_vs_spock/entities/SlowClientSpec.groovy
+++ src/test/groovy/io/github/aplotnikov/junit5_vs_spock/entities/SlowClientSpec.groovy
@@ -1,11 +1,17 @@
 package io.github.aplotnikov.junit5_vs_spock.entities
 
 import static java.math.BigDecimal.TEN
+import static java.util.concurrent.Executors.newSingleThreadScheduledExecutor
 
+import spock.lang.Specification
 import spock.lang.Subject
 import spock.lang.Timeout
+import spock.util.concurrent.AsyncConditions
+import spock.util.concurrent.PollingConditions
 
-class SlowClientSpec {
+import java.util.concurrent.ExecutorService
+
+class SlowClientSpec extends Specification {
 
     @Subject
     Client client = new Client('Andrii', 'Plotnikov', ['test@gmail.com', 'test2@gmail.com'])
@@ -16,4 +22,41 @@ class SlowClientSpec {
             client.pay(TEN)
     }
 
+    void 'client should pay identification fee in max 2 second'() {
+        given:
+            AsyncConditions conditions = new AsyncConditions()
+        when:
+            startPaymentOfIdentificationFee {
+                conditions.evaluate {
+                    assert client.identified
+                }
+            }
+        then:
+            conditions.await(2)
+    }
+
+    void 'client should pay identification fee in max 2 second - 2'() {
+        given:
+            PollingConditions conditions = new PollingConditions(timeout: 2)
+        when:
+            startPaymentOfIdentificationFee()
+        then:
+            conditions.eventually {
+                client.identified
+            }
+    }
+
+    private void startPaymentOfIdentificationFee() {
+        startPaymentOfIdentificationFee {}
+    }
+
+    private void startPaymentOfIdentificationFee(Closure<?> callback) {
+        ExecutorService executorService = newSingleThreadScheduledExecutor()
+        try {
+            executorService.submit { client.payIdentificationFee() }
+            executorService.submit { callback.run() }
+        } finally {
+            executorService.shutdown()
+        }
+    }
 }
